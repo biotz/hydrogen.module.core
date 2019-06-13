5a4abd1ff5c1d88f96f78bf1dc65a400f1f77239
Damian Hryniewicz <damian@magnet.coop>
2019-06-13 16:09:02 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hydrogen/module.core "0.1.4"
+(defproject hydrogen/module.core "0.1.5"
   :description "Duct module for doing cljs-based SPA applications the Hydrogen way."
   :url "https://github.com/magnetcoop/hydrogen.module.core"
   :license {:name "Mozilla Public Licence 2.0"
